bb70982a62536e939798a0615eb26f77167527cd
Abhishek Mane <abhishekmane@outlook.in>
2018-07-12 09:46:31 +0530

    entrypoint
    Signed-off-by: Abhishek Mane <maneabhi@in.ibm.com>

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
-#include "ds/linkedlist/singly_linked_list.c"
+
+#include "ds/linkedlist/headers/singly_linked_list.h"
 
 int main() {
-    printf("Hello, World!\n");
+    display();
     return 0;
 }
