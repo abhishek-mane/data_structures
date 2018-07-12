670e3014cfb2643808c0b32e309d73d2698d5cb6
Abhishek Mane <abhishekmane@outlook.in>
2018-07-12 09:46:16 +0530

    Added singly linked list headers
    Signed-off-by: Abhishek Mane <maneabhi@in.ibm.com>

---
+++ ds/linkedlist/headers/singly_linked_list.h
@@ -0,0 +1,11 @@
+//
+// Created by abhishek on 11/7/18.
+//
+
+#ifndef DATA_STRUCTURES_SINGLY_LINKED_LIST_C_H
+#define DATA_STRUCTURES_SINGLY_LINKED_LIST_C_H
+
+// Declarations
+void display();
+
+#endif //DATA_STRUCTURES_SINGLY_LINKED_LIST_C_H
